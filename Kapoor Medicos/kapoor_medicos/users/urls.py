96ed74d08bd7c8e1a9fec3b389ccbf0870ebc84b
KapoorMedicos <kapoormedicos77766@gmail.com>
2025-03-04 00:57:11 +0530

    Delete Kapoor Medicos directory

--- Kapoor Medicos/kapoor_medicos/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import LoginView, RegisterView  # Ensure these are imported
-
-urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),  # Use as_view()
-    path('register/', RegisterView.as_view(), name='register'),  # Use as_view()
-]
